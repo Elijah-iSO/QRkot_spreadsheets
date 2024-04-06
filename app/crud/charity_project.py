4ca3cd62b19d0ca687a41318bfcfb284ac998b9b
Илья Олейников <oleynikovis@yandex.ru>
2024-04-06 11:49:38 +0300

    job done

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -22,5 +22,33 @@ class CRUDCharityProject(CRUDBase):
         db_project_id = db_project_id.scalars().first()
         return db_project_id
 
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession,
+    ) -> list[dict[str, str]]:
+        stmt = (
+            select(
+                CharityProject.name,
+                CharityProject.description,
+                CharityProject.create_date,
+                CharityProject.close_date
+            ).where(CharityProject.fully_invested == 1)
+        )
+
+        projects = await session.execute(stmt)
+        projects = projects.all()
+
+        final_projects = []
+        for project in projects:
+            time = project.close_date - project.create_date
+            final_projects.append(
+                {
+                    'name': project.name,
+                    'description': project.description,
+                    'time': time
+                }
+            )
+        return sorted(final_projects, key=lambda p: p['time'])
+
 
 charity_project_crud = CRUDCharityProject(CharityProject)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
+from fastapi import FastAPI
+
 from app.api.routers import main_router
 from app.core.config import settings
-from fastapi import FastAPI
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.description,
-    docs_url=None,
+    # docs_url=None,
     redoc_url=None,
 )
 
